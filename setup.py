d229f35422a55099ef1587ed0b59078476e72019
Brian McAllister <mcallb@gmail.com>
2016-09-12 21:51:34 -0400

    Added setup.py and moved folders around to create a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='live_stream',
+      version='0.1',
+      description='Monitors a Youtube live stream',
+      url='https://github.com/mcallb/live_stream',
+      author='Brian McAllister',
+      author_email='mcallb@gmail.com',
+      license='MIT',
+      packages=['live_stream'],
+      zip_safe=False)

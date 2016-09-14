9ce92e437901b3e392f438f477f04b06ed246a97
Brian McAllister <mcallb@gmail.com>
2016-09-14 19:34:56 -0400

    clean up

--- live_stream/main.py
+++ live_stream/main.py
@@ -5,6 +5,10 @@ from live_stream.getstream import GetStream
 import threading
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c','--client_secret', help='The fully qualified path to your google api client_secret.json', required=True)
+parser.add_argument('-x','--xively_secret', help='The fully qualified path to your xively api client_secret.json', required=True)
+args = vars(parser.parse_args())
 
 def main():
     threading.Timer(60.0, main).start()
@@ -14,3 +18,6 @@ def main():
     myXively.send_request(status)
 
 main()
+
+if __name__ == "__main__":
+    main()
